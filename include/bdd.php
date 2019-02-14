609eb11a9691d9f33559beb2149e2472bd46b73d
LordPompax <capitaine_ted@hotmail.fr>
2019-02-14 21:41:26 +0100

    maj include/bdd.php

--- include/bdd.php
+++ include/bdd.php
@@ -1,14 +1,14 @@
 <?php
 	try{
-		$bdd = new PDO('mysql:host=localhost;dbname=pompier','root',''); // connxion a la bdd
+		$bdd = new PDO('mysql:host=localhost;dbname=pompier','root',''); 	// connxion a la bdd
 		$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
 	catch(Exception $e){
 		die('Erreur : '.$e->getMessage());
 	}
 	
-	$domain = 'http://192.168.1.24/pompier'; // nom de domaine
-	
+	//$domain = 'http://192.168.1.24/pompier'; 
+	$domain = 'http://localhost'; 											// nom de domaine (dépand de la ou sont les fichiers)
 	
 	//ini_set("display_errors",0);error_reporting(0);
 ?>
