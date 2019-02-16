01334162dbd42e24ec330a44a6ec636b506ffddc
root <root@pc-24.home>
2019-02-16 02:25:57 +0100

    maj interface d'accueil

--- accueil/index.php
+++ accueil/index.php
@@ -26,58 +26,73 @@
 					<div class = "princip">
 						<div class = "flex">
 							<div class = "fen spe">
-								<table class = "tab_info">
-									<tr>
-										<th class = "ligne_tab_info">de</th>
-										<th class = "ligne_tab_info">a</th>
-										<th class = "ligne_tab_info">dispo</th>
-									</tr>
-									<tr>
-										<?php
-											$remp = $bdd->query('SELECT * FROM remplacement'); 								// recherche tout les tuples (lignes) remplacements
-											while ($infoRemp = $remp->fetch()) { 											// enregistre dans un tableau les tuples recherché
-												echo '
-													<td class = "ligne_tab_info">'.substr($infoRemp['de'], 5, 11).'</td> 
-													<td class = "ligne_tab_info">'.substr($infoRemp['a'], 5, 11).'</td>
-													<td class = "ligne_tab_info">'.$infoRemp['dispo'].'</td>
-												'; 																			// affiche les attributs (colonnes) de chaque tuples dans des balises html
-											}
-										?>
-									</tr>
-								</table>
+								<div class = "fen_titre">remplacement</div>
+								<div style = "padding : 10px;">
+									<table class = "tab_info">
+										<tr>
+											<th class = "ligne_tab_info couleur_tab_ligne">de</th>
+											<th class = "ligne_tab_info couleur_tab_ligne">a</th>
+											<th class = "ligne_tab_info couleur_tab_ligne">dispo</th>
+										</tr>
+											<?php
+												$remp = $bdd->query('SELECT * FROM remplacement'); 								// recherche tout les tuples (lignes) remplacements
+												while ($infoRemp = $remp->fetch()) { 											// enregistre dans un tableau les tuples recherché
+													echo '
+														<tr>
+															<td class = "ligne_tab_info">'.substr($infoRemp['de'], 5, 11).'</td> 
+															<td class = "ligne_tab_info">'.substr($infoRemp['a'], 5, 11).'</td>
+															<td class = "ligne_tab_info">'.$infoRemp['dispo'].'</td>
+														</tr>
+													'; 																			// affiche les attributs (colonnes) de chaque tuples dans des balises html
+												}
+											?>
+									</table>
+								</div>
 							</div>
 							<div class = "fen spe">
-								<table class = "tab_info">
-									<tr>
-										<th class = "ligne_tab_info">de</th>
-										<th class = "ligne_tab_info">a</th>
-										<th class = "ligne_tab_info">dispo</th>
-									</tr>
-									<tr>
-										<?php
-											$inter = $bdd->query('SELECT * FROM intervention');
-											while ($infoInter = $inter->fetch()) {
-												echo '
-													<td class = "ligne_tab_info">'.substr($infoInter['quand'], 5, 11).'</td>
-													<td class = "ligne_tab_info">'.$infoInter['motif'].'</td>
-													<td class = "ligne_tab_info">'.$infoInter['dispo'].'</td>
-												';
-											}
-										?>
-									</tr>
-								</table>
+								<div class = "fen_titre">intervention</div>
+								<div style = "padding : 10px;">
+									<table class = "tab_info">
+										<tr>
+											<th class = "ligne_tab_info couleur_tab_ligne">de</th>
+											<th class = "ligne_tab_info couleur_tab_ligne">motif</th>
+											<th class = "ligne_tab_info couleur_tab_ligne">dispo</th>
+										</tr>
+										<tr>
+											<?php
+												$inter = $bdd->query('SELECT * FROM intervention');
+												while ($infoInter = $inter->fetch()) {
+													echo '
+														<td class = "ligne_tab_info">'.substr($infoInter['quand'], 5, 11).'</td>
+														<td class = "ligne_tab_info">'.$infoInter['motif'].'</td>
+														<td class = "ligne_tab_info">'.$infoInter['dispo'].'</td>
+													';
+												}
+											?>
+										</tr>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
 					<div class = "second">
 						<div class = "fen">
-							lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol
+							<div class = "fen_titre">test</div>
+							<div style = "padding : 10px;">
+								lolollolollolollolollolollolollolollolollolollolollolol
+							</div>
 						</div>
 						<div class = "fen">
-							lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol
+							<div class = "fen_titre">test</div>
+							<div style = "padding : 10px;">
+								lolollolollolollolollolollolollolollolollolollolollolol
+							</div>
 						</div>
 						<div class = "fen">
-							lollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollollol
+							<div class = "fen_titre">test</div>
+							<div style = "padding : 10px;">
+								lolollolollolollolollolollolollolollolollolollolollolol
+							</div>
 						</div>
 					</div>
 				</div>
